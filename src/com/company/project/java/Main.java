287442a5cedd0085ae9be319bd7a6ea2faa06819
Will Burke <billyburke1012@gmail.com>
2021-03-01 18:28:12 -0500

    first java commit

---
+++ src/com/company/project/java/Main.java
@@ -0,0 +1,26 @@
+package com.company.project.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello, World!");
+	    System.out.println("Testing Testing 123");
+
+	    int pets = 0;
+	    double coffeeCost = 4.44;
+	    double latitude = 33;
+	    double longitude = -84;
+	    var firstInitial = "W";
+	    var lastInitial = "B";
+	    String tvShow = "Super Store";
+
+	    System.out.println("Hello World " + firstInitial + lastInitial);
+	    System.out.println("I have " + pets +" pets.");
+	    System.out.println("I paid about $" + coffeeCost + " for coffee this morning.");
+	    System.out.println("My current location is: " + latitude + ", " + longitude);
+	    System.out.println("My initials are " + firstInitial + "." + lastInitial + ".");
+	    System.out.println("I have been watching " + tvShow + " and recommend you watch also!");
+    }
+}
